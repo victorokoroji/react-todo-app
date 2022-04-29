6c31f812364a3145181719f28679ab745ccca956
unknown <okorojiebube2@gmail.com>
2022-04-29 12:49:46 -0600

    Add toggle style

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,20 +4,29 @@ import Input from './Input'
 import styles from './TodoItem.module.css'
 
 const TodoItem = ({ todo, handlePropsChange, deleteTodoProps }) => {
+	const completedStyle = {
+		fontStyle: 'italic',
+		color: '#595959',
+		opacity: 0.4,
+		textDecoration: 'line-through',
+	}
 	return (
 		<>
 			<li className={styles.item}>
 				<Input
 					type='checkbox'
 					name='checkbox'
-					className={styles.checkbox}s
+					className={styles.checkbox}
+					s
 					checked={todo.completed}
 					onChange={() => handlePropsChange(todo.id)}
 				/>
-				{todo.title}
 				<Button type='button' onClick={() => deleteTodoProps(todo.id)}>
 					Delete
 				</Button>
+				<span style={this.props.todo.completed ? completedStyle : null}>
+					{this.props.todo.title}
+				</span>
 			</li>
 		</>
 	)
